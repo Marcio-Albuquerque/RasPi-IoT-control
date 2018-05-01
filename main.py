a3e2772adb6395f30eb1253bde0719bb549c0c3b
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2018-05-01 00:29:51 +0000

    Start project

---
+++ main.py
@@ -0,0 +1,18 @@
+#     /\\---/\\   ********************************************   
+#    /^   ^  \\   *    Program for controlling an air pump   * 
+#   ( O   O   )   *       Ant. Marcio A. A. 2018             *  
+#    `.=o=__,'    *     o.marcio.albu@gmail.com              *  
+#                 ********************************************     
+
+####
+# Main Control Structure
+####
+
+#*#*#*#*#
+# * TODO
+# V 0.1
+# * Implement on / off motor DC with object-oriented python;
+# * Implement button push On/ Off;
+# * Implement LCD;
+# * Implement simple graphical interface.
+#*#*#*#*#
