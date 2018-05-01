60fd7a03cd5a229c9a4b1f9fcc513129ade5b586
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2018-05-01 11:06:16 +0000

    Implementing on / off motor DC with object-oriented python

--- main.py
+++ main.py
@@ -11,8 +11,25 @@
 #*#*#*#*#
 # * TODO
 # V 0.1
-# * Implement on / off motor DC with object-oriented python;
+# * Implement on / off motor DC with object-oriented python (Check);
 # * Implement button push On/ Off;
 # * Implement LCD;
 # * Implement simple graphical interface.
 #*#*#*#*#
+
+from motor import Motor
+
+objMotor = Motor(False) #Default startup
+
+try: #Interruption Structure
+    while True:
+        button = input("Start motor (Y/N) \n--> ")
+        if button == "Y":
+            objMotor.setStateMotor(True)
+            objMotor.setActionMotor()
+        else:
+            objMotor.setStateMotor(False)
+            objMotor.setActionMotor()
+except KeyboardInterrupt: #Interruption Structure
+    pass
+    
--- motor.py
+++ motor.py
@@ -7,3 +7,34 @@
 ####
 # Code for control motor DC
 ####
+
+##Libraries
+import RPi.GPIO as GPIO
+from time import sleep
+
+##Configuration of GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+motorPIN = 7 #Pin out
+
+GPIO.setup(motorPIN,GPIO.OUT) #Pin setup
+
+class Motor():
+
+    def __init__(self, bool):
+        self.state = bool
+
+    def getStateMotor(self):#If true On, if False Off
+        return self.state
+
+    def setStateMotor(self, bool):
+        self.state = bool
+
+    def setActionMotor(self):
+        if self.state:
+            print('Start')
+            GPIO.output(motorPIN,GPIO.HIGH)
+        else:
+            GPIO.output(motorPIN,GPIO.LOW)
+            print('Stop')
