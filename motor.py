2495bf392c6d09472176fff82b645ae769d8fa76
Marcio-Albuquerque <marcio.albu@alu.ufc.br>
2018-05-01 00:36:02 +0000

    Edit file

---
+++ motor.py
@@ -0,0 +1,9 @@
+#     /\\---/\\   ********************************************   
+#    /^   ^  \\   *    Program for controlling an air pump   * 
+#   ( O   O   )   *       Ant. Marcio A. A. 2018             *  
+#    `.=o=__,'    *     o.marcio.albu@gmail.com              *  
+#                 ********************************************     
+
+####
+# Code for control motor DC
+####
